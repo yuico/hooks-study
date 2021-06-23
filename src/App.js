ca77acbaceb6a0d5adb67e9b6bba18f479f09c86
nokdoo00 <nokdoo0@gmail.com>
2021-06-22 22:40:08 -0400

    feat: add ref.current.focus to focus on the input field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
 import Parent from './components/parent';
 
 function App() {
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const submitRef = useRef(null);
+  useEffect(() => {
+    firstNameRef.current.focus();
+  }, []);
+
   return (
     <div className='App'>
+      <header className='App-header'>
+        <input
+          type='text'
+          onKeyDown={firstKeyDown}
+          ref={firstNameRef}
+          placeholder='Enter first name'
+        />
+        <input
+          type='text'
+          onKeyDown={lastKeyDown}
+          ref={lastNameRef}
+          placeholder='Enter last name'
+        />
+        <button onKeyDown={submitKeyDown} ref={submitRef}>
+          Submit
+        </button>
+      </header>
       <Parent />
     </div>
   );
